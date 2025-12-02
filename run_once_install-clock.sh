5355565c7310d60cbf1ad9439dd16fdd125395e0
ioan <ioanb7code@gmail.com>
2025-12-02 20:56:03 +0000

    Settings, clock, filezilla

---
+++ run_once_install-clock.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+echo "Installing GNOME Clocks..."
+
+# Install code
+sudo apt install -y gnome-clocks
+
+echo "GNOME Clocks installation complete!"
---
+++ run_once_install-filezilla.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+echo "Installing Filezilla..."
+
+# Install code
+sudo apt install -y filezilla
+
+echo "Filezilla installation complete!"
